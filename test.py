36759ef6d0dee11cacb96e4de164572a662efeab
EmiMontanoC <emimontano1327@gmail.com>
2026-08-20 17:46:10 -0700

    Added default values

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ def main():
     Main function to parse command line arguments and calculate the area.
     """
     parser = argparse.ArgumentParser(description="Calculate the area of a rectangle ")
-    parser.add_argument("-l", "--length", type=float, required=True, help="Length of the rectangle")
-    parser.add_argument("-w", "--width", type=float, required=True, help="Width of the rectangle")
+    parser.add_argument("-l", "--length",default=10.0 , type=float, required=True, help="Length of the rectangle")
+    parser.add_argument("-w", "--width",default=5.0 , type=float, required=True, help="Width of the rectangle")
     args = parser.parse_args()
+    
     area = calculate_area(args.length, args.width)
     print(f"The area of the rectangle is: {area}") 
     
